c7f1b80a86881fecae8bc3572a1a75295a07a570
Matteo <matteoruda@toidigitalagency.it>
2021-08-12 15:59:51 +0200

    Different entrypoint for datatable

---
+++ assets/js/datatables.js
@@ -0,0 +1,8 @@
+//Datatables
+require('datatables.net-bs4/js/dataTables.bootstrap4.min');
+require('datatables.net-buttons-bs4/js/buttons.bootstrap4.min');
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
+import 'datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css';
+
+const datatableTranslation = require('../json/datatableItalian.json');
+window.datatableTranslation = datatableTranslation;
