795fdb789bdde585f007930f5beabc40561242c0
Matteo <matteoruda@toidigitalagency.it>
2021-08-17 12:09:46 +0200

    Added profile page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'title' => 'Pagina del profilo',
+            'description' => 'Visualizza e gestisci il tuo profilo',
+            'user' => $this->getUser()
+        ]);
+    }
+}
