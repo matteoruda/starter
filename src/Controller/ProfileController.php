ae8d89150c3d9342f0d527c3ed578aa309a43dba
Matteo <matteoruda@toidigitalagency.it>
2021-08-17 13:20:38 +0200

    Added password change in profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfileController extends AbstractController
 {
     /**
      * @Route("/profile", name="profile")
      */
-    public function index(): Response
+    public function index(Request $request, UserInterface $user, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
+        $passwordForm = $this->createForm(ChangePasswordType::class);
+
+        $passwordForm->handleRequest($request);
+
+        if($passwordForm->isSubmitted() && $passwordForm->isValid())
+        {
+            $password = $passwordForm->get('password')->getData();
+            $user->setPassword( $passwordHasher->hashPassword($user, $password) );
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Password cambiate con successo');
+        }
+
         return $this->render('profile/index.html.twig', [
             'title' => 'Pagina del profilo',
             'description' => 'Visualizza e gestisci il tuo profilo',
-            'user' => $this->getUser()
+            'passwordForm' => $passwordForm->createView(),
+            'user' => $user
         ]);
     }
 }
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'La nuova password non corrisponde con la password di conferma',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Salva',
+                'attr' => [
+                    'class' => 'btn btn-primary float-right'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
